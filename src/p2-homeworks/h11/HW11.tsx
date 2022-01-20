6b1abe752b4d3162fb912b08bec2db2d526b6838
ole4ka140592 <ole4ka140592@gmail.com>
2022-01-20 22:57:12 +0300

    add homework 11, css

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,11 +1,13 @@
 import React, {ChangeEvent, useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
+import classes from "./HW11.module.css";
+
 
 
 function HW11() {
-    const [value1, setValue1] = useState(11)
-    const [value2, setValue2] = useState(90)
+    const [value1, setValue1] = useState(0)
+    const [value2, setValue2] = useState(100)
 
     const onChangeRangeHandler1 = (value: number) => {
         setValue1(value)
@@ -18,22 +20,22 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange onChangeRange={onChangeRangeHandler1} value1={value1}
+            <div className={classes.superRange}>
+                <div className={classes.superValue1}><span>{value1}</span></div>
+                <div className={classes.range}><SuperRange onChangeRange={onChangeRangeHandler1} value1={value1}
                     // сделать так чтоб value1 изменялось
-                />
+                /></div>
             </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
+            <div className={classes.superDoubleRange}>
+                <div className={classes.doubleValue1}><span>{value1}</span></div>
+                <div className={classes.doubleRange}><SuperDoubleRange
                     onChangeRange={onChangeRangeHandler1}
                     value1={value1}
                     value2={value2}
 
-                />
-                <span>{value2}</span>
+                /></div>
+                <div className={classes.doubleValue2}><span>{value2}</span></div>
             </div>
 
             <hr/>
